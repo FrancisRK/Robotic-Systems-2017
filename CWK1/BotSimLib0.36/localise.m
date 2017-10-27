e53be900900bb24a0e671fcf916e91f4deeff567
Francis Riazi Kermani <fr14161@bristol.ac.uk>
2017-10-27 19:30:03 +0100

    Saving progress while Vesko wants to play table tennis, includes code to create mapMatrix which will keep particles and robot in bounds of map

--- CWK1/BotSimLib0.36/localise.m
+++ CWK1/BotSimLib0.36/localise.m
@@ -8,10 +8,15 @@ map = [0,0;60,0;60,45;45,45;45,59;106,59;106,105;0,105];  %default map
 modifiedMap = map; %you need to do this modification yourself
 botSim.setMap(modifiedMap);
 
+
+%%% Assuming all maps have limsMin = [0 0] -- Could possibly change
+limsMax = max(map); % maximum limits of the map
+
+mapMatrix = zeros(limsMax + [1,1]);
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-botSim = BotSim(map);  %sets up a botSim object a map, and debug mode on.
-botSim.drawMap();
-botSim.drawBot(5);
+%botSim = BotSim(map);  %sets up a botSim object a map, and debug mode on.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %generate some random particles inside the map
@@ -20,6 +25,7 @@ particles(num,1) = BotSim; %how to set up a vector of objects
 for i = 1:num
     particles(i) = BotSim(modifiedMap);  %each particle should use the same map as the botSim object
     particles(i).randomPose(5); %spawn the particles in random locations
+    if particles(i).
     particles(i).drawBot(3);
 end
 
@@ -30,11 +36,11 @@ n = 0;
 converged = 0; %The filter has not converged yet
 while(converged == 0 && n < maxNumOfIterations) %%particle filter loop
     n = n+1; %increment the current number of iterations
-    [distances crossingPoints] = botSim.ultraScan(); %get a scan from the real robot.
+    [distances, crossingPoints] = botSim.ultraScan(); %get a scan from the real robot.
     
-    botSim.drawScanConfig(); %draws current scan configuration
-    botSim.drawBot(3);
-    scatter(crossingPoints(:,1),crossingPoints(:,2),'marker','o','lineWidth',3); %draws crossingpoints
+    distances
+    crossingPoints
+    
     %% Write code for updating your particles scans
     
     for i = 1:num
@@ -47,6 +53,7 @@ while(converged == 0 && n < maxNumOfIterations) %%particle filter loop
     
     %% Write code for resampling your particles
     
+    %if particles(i)
     
     %% Write code to check for convergence   
 	
@@ -57,7 +64,7 @@ while(converged == 0 && n < maxNumOfIterations) %%particle filter loop
     %% Write code to decide how to move next
     % here they just turn in cicles as an example
     turn = 0.5;
-    move = 2;
+    move = 0;
     botSim.turn(turn); %turn the real robot.  
     botSim.move(move); %move the real robot. These movements are recorded for marking 
     for i =1:num %for all the particles. 
@@ -71,6 +78,8 @@ while(converged == 0 && n < maxNumOfIterations) %%particle filter loop
         hold off; %the drawMap() function will clear the drawing when hold is off
         botSim.drawMap(); %drawMap() turns hold back on again, so you can draw the bots
         botSim.drawBot(30,'g'); %draw robot with line length 30 and green
+        botSim.drawScanConfig(); %draws current scan configuration
+        scatter(crossingPoints(:,1),crossingPoints(:,2),'marker','o','lineWidth',3); %draws crossingpoints
         for i =1:num
             particles(i).drawBot(3); %draw particle with line length 3 and default color
         end
